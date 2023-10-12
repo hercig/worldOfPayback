a985badc41a0240dee5cd2e52677e3b3140a20d3
Filip Hercig <filip.hercig@appsfactory.de>
2023-10-12 09:54:39 +0200

    Initial Commit

---
+++ WorldOfPAYBACK/WorldOfPAYBACKApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorldOfPAYBACKApp.swift
+//  WorldOfPAYBACK
+//
+//  Created by Filip Hercig on 12.10.23.
+//
+
+import SwiftUI
+
+@main
+struct WorldOfPAYBACKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
